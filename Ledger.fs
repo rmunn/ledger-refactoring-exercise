7145f78c815118ca3198a062ad2dc62a36300d5c
Robin Munn <rmunn@pobox.com>
2016-09-11 08:18:01 +0900

    Ledger refactor, step 6

--- Ledger.fs
+++ Ledger.fs
@@ -19,7 +19,7 @@ module Resource =
         "Date", "Date"
         "Description", "Description"
         "Change", "Change"
-        "*DateFormat*", "MM\/dd\/yyyy"  // TODO: Are these backslashes needed?
+        "*DateFormat*", "MM/dd/yyyy"
         "*NumberFormat*", "#,#0.00"
       ] |> Map.ofList
 
@@ -35,15 +35,53 @@ module Resource =
     let lookupTable = function
         | "en-US" -> englishStrings
         | "nl-NL" -> dutchStrings
+        | _ -> englishStrings // Untranslated locales default to English
 
     let lookupStr key locale =
         locale |> lookupTable |> Map.find key
+
+[<AutoOpen>]
+module MoneyFormatting =
+    let toCurrencySymbol = function
+        | "USD" -> "$"
+        | "EUR" -> "€"
+        | other -> other
+
+    let fmtCurrencySymbol currency locale =
+        let symbol = currency |> toCurrencySymbol
+        match locale with
+        | "en-US" -> symbol
+        | "nl-NL" -> sprintf "%s " symbol
+        | _ -> symbol // Unrecognized locales default to just using the symbol
+
+    let fmtNegativeNumber = function
+        | "en-US" -> fun (s:string) -> s.Replace("-", "") |> sprintf "(%s)" 
+        | "nl-NL" -> id
+        | _ -> id // Unrecognized locales default to not changing the input
+
+    let fmtPositiveNumber = function
+        | "en-US"
+        | "nl-NL" -> fun s -> sprintf "%s " s
+        | _ -> id // Unrecognized locales default to not changing the input
+
+    let fmtChange currency locale (amt:int) =
+        let chg = float amt / 100.0
+        let symbol = fmtCurrencySymbol currency locale 
+        let fmtStr = lookupStr "*NumberFormat*" locale
+        let s = symbol + chg.ToString(fmtStr, new CultureInfo(locale))
+        if chg >= 0.0 then fmtPositiveNumber locale s
+                      else fmtNegativeNumber locale s
 
 let padOrTrim width (str:string) =
     if str.Length <= width then
         str.PadRight(width)
     else
         str.Substring(0, width-3) + "..."
+
+let padLeft width (str:string) = str.PadLeft(width)
+
+let formatChangeW width currency locale amt =
+    fmtChange currency locale amt |> padLeft width
 
 let formatDate (date:DateTime) locale =
     date.ToString(lookupStr "*DateFormat*" locale)
@@ -71,28 +109,7 @@ let formatLedger currency locale entries =
         res <- res + padOrTrim 25 x.des
 
         res <- res + " | "
-        let c = float x.chg / 100.0
 
-        if c < 0.0 then 
-            if locale = "nl-NL" then
-                if currency = "USD" then
-                    res <- res + ("$ " + c.ToString("#,#0.00", new CultureInfo("nl-NL"))).PadLeft(13) 
-                if currency = "EUR" then
-                    res <- res + ("€ " + c.ToString("#,#0.00", new CultureInfo("nl-NL"))).PadLeft(13) 
-            if locale = "en-US" then
-                if currency = "USD" then
-                    res <- res + ("($" + c.ToString("#,#0.00", new CultureInfo("en-US")).Substring(1) + ")").PadLeft(13) 
-                if currency = "EUR" then
-                    res <- res + ("(€" + c.ToString("#,#0.00", new CultureInfo("en-US")).Substring(1) + ")").PadLeft(13) 
-        else 
-            if locale = "nl-NL" then
-                if currency = "USD" then
-                    res <- res + ("$ " + c.ToString("#,#0.00", new CultureInfo("nl-NL")) + " ").PadLeft(13) 
-                if currency = "EUR" then
-                    res <- res + ("€ " + c.ToString("#,#0.00", new CultureInfo("nl-NL")) + " ").PadLeft(13) 
-            if locale = "en-US" then
-                if currency = "USD" then
-                    res <- res + ("$" + c.ToString("#,#0.00", new CultureInfo("en-US")) + " ").PadLeft(13) 
-                if currency = "EUR" then
-                    res <- res + ("€" + c.ToString("#,#0.00", new CultureInfo("en-US")) + " ").PadLeft(13) 
+        res <- res + formatChangeW 13 currency locale x.chg
+
     res
